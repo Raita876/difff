785487eccc0b2076d9e6c93fce43d6af0506e6f9
Raita Watanabe <watanaberit20171129@gmail.com>
2024-07-07 12:55:25 +0900

    [fix] e2e script

--- e2e/test.sh
+++ e2e/test.sh
@@ -14,26 +14,30 @@ readonly TARGET_DIR_PATH="${THIS_SCRIPT_DIR}/data/target"
 WANT=$(
     cat <<EOS
 {
-  "source": [
-    {
-      "path": "e/f/g.txt",
-      "hash": "211c102123b4a41bd5227dcc84952349"
-    },
-    {
-      "path": "e/i.txt",
-      "hash": "1b08ef3ea73ce6fd8b2ef57f54073b5a"
-    }
-  ],
-  "target": [
-    {
-      "path": "e/f/g.txt",
-      "hash": "e4727cb9315a4fddec71e1a85cad6c09"
-    },
-    {
-      "path": "e/f/j.txt",
-      "hash": "f6c79025f3b5bedac7cd769f0847e36a"
-    }
-  ]
+  "source": "${SOURCE_DIR_PATH}",
+  "target": "${TARGET_DIR_PATH}",
+  "diff": {
+    "source": [
+      {
+        "path": "e/f/g.txt",
+        "hash": "211c102123b4a41bd5227dcc84952349"
+      },
+      {
+        "path": "e/i.txt",
+        "hash": "1b08ef3ea73ce6fd8b2ef57f54073b5a"
+      }
+    ],
+    "target": [
+      {
+        "path": "e/f/g.txt",
+        "hash": "e4727cb9315a4fddec71e1a85cad6c09"
+      },
+      {
+        "path": "e/f/j.txt",
+        "hash": "f6c79025f3b5bedac7cd769f0847e36a"
+      }
+    ]
+  }
 }
 EOS
 )
@@ -47,11 +51,8 @@ function main() {
             "${TARGET_DIR_PATH}"
     )
 
-    local result_diff
-    result_diff=$(echo "${result}" | jq '.diff')
-
     diff -u \
-        <(echo "${result_diff}") \
+        <(echo "${result}") \
         <(echo "${WANT}")
 
 }
